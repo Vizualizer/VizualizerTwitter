99a6909f273ca430216173f461e41e13049f6def
Naohisa Minagawa <info@clay-system.jp>
2014-05-11 09:23:07 +0900

    検索条件をまとめずに個別で検索するように修正

--- classes/VizualizerTwitter/Batch/SearchFollowAccounts.php
+++ classes/VizualizerTwitter/Batch/SearchFollowAccounts.php
@@ -94,87 +94,89 @@ class VizualizerTwitter_Batch_SearchFollowAccounts extends Vizualizer_Plugin_Bat
             }
 
             // ユーザー情報を検索
-            for ($i = 0; $i < 10; $i ++) {
-                $twitter = \Codebird\Codebird::getInstance();
-                $twitter->setToken($account->access_token, $account->access_token_secret);
+            for ($i = 0; $i < 3; $i ++) {
+                foreach($keywords as $keyword){
+                    $twitter = \Codebird\Codebird::getInstance();
+                    $twitter->setToken($account->access_token, $account->access_token_secret);
 
-                if($i < 2){
-                    $page = $i + 1;
-                }else{
-                    $page = mt_rand(3, 50);
-                }
-                $users = (array) $twitter->users_search(array("q" => implode(" ", $keywords), "page" => $page, "per_page" => 20));
-                unset($users["httpstatus"]);
-                echo "Search Users（".count($users)."） in page ".$page."\r\n";
-                foreach ($users as $index => $user) {
-                    // ユーザーのIDが取得できない場合はスキップ
-                    if(!($user->id > 0)){
-                        echo "Skipped invalid ID : ".$user->id." in ".$index."\r\n";
-                        print_r($user);
-                        continue;
+                    if($i < 2){
+                        $page = $i + 1;
+                    }else{
+                        $page = mt_rand(3, 50);
                     }
+                    $users = (array) $twitter->users_search(array("q" => $keyword, "page" => $page, "per_page" => 20));
+                    unset($users["httpstatus"]);
+                    echo "Search Users（".count($users)."） in page ".$page."\r\n";
+                    foreach ($users as $index => $user) {
+                        // ユーザーのIDが取得できない場合はスキップ
+                        if(!($user->id > 0)){
+                            echo "Skipped invalid ID : ".$user->id." in ".$index."\r\n";
+                            print_r($user);
+                            continue;
+                        }
 
-                    // 日本語チェックに引っかかる場合はスキップ
-                    if ($account->japanese_flg == "1" && $user->lang != "ja") {
-                        echo "Skipped invalid not Japanese : ".$user->id."\r\n";
-                        continue;
-                    }
+                        // 日本語チェックに引っかかる場合はスキップ
+                        if ($account->japanese_flg == "1" && $user->lang != "ja") {
+                            echo "Skipped invalid not Japanese : ".$user->id."\r\n";
+                            continue;
+                        }
 
-                    // ボットチェックに引っかかる場合はスキップ
-                    if ($account->non_bot_flg == "1" && preg_match("/BOT|ボット|ﾎﾞｯﾄ/ui", $user->description) > 0) {
-                        echo "Skipped invalid Bot : ".$user->id."\r\n";
-                        continue;
-                    }
+                        // ボットチェックに引っかかる場合はスキップ
+                        if ($account->non_bot_flg == "1" && preg_match("/BOT|ボット|ﾎﾞｯﾄ/ui", $user->description) > 0) {
+                            echo "Skipped invalid Bot : ".$user->id."\r\n";
+                            continue;
+                        }
 
-                    // 拒絶キーワードを含む場合はスキップ
-                    if (!empty($account->ignore_keywords) && preg_match("/" . implode("|", explode("\r\n", $account->ignore_keywords)) . "/u", $user->description) > 0) {
-                        echo "Skipped invalid Profile : ".$user->id."\r\n";
-                        continue;
-                    }
+                        // 拒絶キーワードを含む場合はスキップ
+                        if (!empty($account->ignore_keywords) && preg_match("/" . implode("|", explode("\r\n", $account->ignore_keywords)) . "/u", $user->description) > 0) {
+                            echo "Skipped invalid Profile : ".$user->id."\r\n";
+                            continue;
+                        }
 
-                    // フォロー対象に追加済みの場合はスキップ
-                    $follow = $loader->loadModel("Follow");
-                    $follow->findBy(array("account_id" => $account->account_id, "user_id" => $user->id));
-                    if ($follow->follow_id > 0) {
-                        echo "Skipped targeted : ".$user->id." was followed\r\n";
-                        continue;
-                    }
+                        // フォロー対象に追加済みの場合はスキップ
+                        $follow = $loader->loadModel("Follow");
+                        $follow->findBy(array("account_id" => $account->account_id, "user_id" => $user->id));
+                        if ($follow->follow_id > 0) {
+                            echo "Skipped targeted : ".$user->id." was followed\r\n";
+                            continue;
+                        }
 
-                    // トランザクションの開始
-                    $connection = Vizualizer_Database_Factory::begin("twitter");
+                        // トランザクションの開始
+                        $connection = Vizualizer_Database_Factory::begin("twitter");
 
-                    try {
+                        try {
+                            // フォロー対象に追加
+                            $follow->account_id = $account->account_id;
+                            $follow->user_id = $user->id;
+                            if(!empty($user->following)){
+                                $follow->friend_date = date("Y-m-d H:i:s");
+                            }
+                            $follow->save();
+                            echo "Add follow target : ".$user->id."\r\n";
+
+                            // フォロー履歴に追加
+                            $history = $loader->loadModel("FollowHistory");
+                            $today = date("Y-m-d");
+                            $history->findBy(array("account_id" => $account->account_id, "history_date" => $today));
+                            $history->account_id = $account->account_id;
+                            $history->history_date = $today;
+                            $history->target_count ++;
+                            $history->save();
+
+                            // リスト無しステータスの場合は待機中ステータスに移行
+                            if($account->follow_status == "4"){
+                                $account->follow_status == "1";
+                                $account->save();
+                            }
+
+                            // エラーが無かった場合、処理をコミットする。
+                            Vizualizer_Database_Factory::commit($connection);
+                        } catch (Exception $e) {
+                            Vizualizer_Database_Factory::rollback($connection);
+                            throw new Vizualizer_Exception_Database($e);
+                        }
                         // フォロー対象に追加
-                        $follow->account_id = $account->account_id;
-                        $follow->user_id = $user->id;
-                        if(!empty($user->following)){
-                            $follow->friend_date = date("Y-m-d H:i:s");
-                        }
-                        $follow->save();
-                        echo "Add follow target : ".$user->id."\r\n";
-
-                        // フォロー履歴に追加
-                        $history = $loader->loadModel("FollowHistory");
-                        $today = date("Y-m-d");
-                        $history->findBy(array("account_id" => $account->account_id, "history_date" => $today));
-                        $history->account_id = $account->account_id;
-                        $history->history_date = $today;
-                        $history->target_count ++;
-                        $history->save();
-
-                        // リスト無しステータスの場合は待機中ステータスに移行
-                        if($account->follow_status == "4"){
-                            $account->follow_status == "1";
-                            $account->save();
-                        }
-
-                        // エラーが無かった場合、処理をコミットする。
-                        Vizualizer_Database_Factory::commit($connection);
-                    } catch (Exception $e) {
-                        Vizualizer_Database_Factory::rollback($connection);
-                        throw new Vizualizer_Exception_Database($e);
                     }
-                    // フォロー対象に追加
                 }
             }
         }
