198dd40e27b1b8e15bab8d0da3a199e9565cb9d6
Naohisa Minagawa <info@clay-system.jp>
2014-07-13 15:57:14 +0900

    #2014071301 指定したグループで検索するためのメソッドを追加

---
+++ classes/VizualizerTwitter/Module/Account/InitializeGroup.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Copyright (C) 2012 Vizualizer All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author    Naohisa Minagawa <info@vizualizer.jp>
+ * @copyright Copyright (c) 2010, Vizualizer
+ * @license http://www.apache.org/licenses/LICENSE-2.0.html Apache License, Version 2.0
+ * @since PHP 5.3
+ * @version   1.0.0
+ */
+
+/**
+ * アカウント検索用にgroup_idを初期化する。
+ *
+ * @package VizualizerTwitter
+ * @author Naohisa Minagawa <info@vizualizer.jp>
+ */
+class VizualizerTwitter_Module_Account_InitializeGroup extends Vizualizer_Plugin_Module
+{
+
+    function execute($params)
+    {
+        // 取得対象のグループIDを調整
+        if(!is_array($post["group_id"])){
+            $post->set("group_id", array());
+        }
+        $groupIds = $post["group_id"];
+        if($post["add_group_id"] > 0){
+            $groupIds[$post["add_group_id"]] = $post["add_group_id"];
+        }
+        if($post["del_group_id"] > 0){
+            unset($groupIds[$post["del_group_id"]]);
+        }
+        $post->set("group_id", $groupIds);
+
+        // グループIDの対象となるアカウントのリストを取得
+        $loader = new Vizualizer_Plugin("Twitter");
+        $model = $loader->loadModel("AccountGroup");
+        $search = $post["search"];
+        $accountIds = $search["in:account_id"];
+        if(!empty($groupIds)){
+            $models = $model->findAllBy(array("in:group_id" => $groupIds));
+            $newAccountIds = array();
+            foreach($models as $model){
+                $newAccountIds[$model->account_id] = $model->account_id;
+            }
+            $accountIds = array_intersect($accountIds, $newAccountIds);
+        }else{
+            // グループ未指定の場合は対象を無しにする。
+            $accountIds = array(0);
+        }
+        $search["in:account_id"] = $accountIds;
+        $post->set("search", $search);
+    }
+}
