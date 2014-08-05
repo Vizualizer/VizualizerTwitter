894150760ea71ea77ba95c0e833163405a6e1ee6
Naohisa Minagawa <info@clay-system.jp>
2014-08-05 20:23:55 +0900

    #2014080502 リツイートの登録で複数登録を許可するようにする。

--- classes/VizualizerTwitter/Module/Retweet/Create.php
+++ classes/VizualizerTwitter/Module/Retweet/Create.php
@@ -48,39 +48,50 @@ class VizualizerTwitter_Module_Retweet_Create extends Vizualizer_Plugin_Module
             $accountIds = array_slice($accountIds, 0, $post["max_accounts"]);
         }
 
+        $tweetIds = array();
+
         // URLから対象のツイートIDを取得する。
         if(preg_match("/^https?:\\/\\/twitter\\.com\\/([a-zA-Z0-9_]+)\\/status\\/([0-9]+)\\/?$/", $post["retweet_target"], $params) > 0){
-            $screenName = $params[1];
-            $tweetId = $params[2];
-        }
-        $tweet = $loader->loadModel("TweetLog");
-        $tweet->findBy(array("twitter_id" => $tweetId));
-
-        // トランザクションの開始
-        $connection = Vizualizer_Database_Factory::begin("twitter");
-        try {
-            foreach($accountIds as $accountId){
-                if($accountId != $tweet->account_id){
-                    $model = $loader->loadModel("Retweet");
-                    $model->account_id = $accountId;
-                    $model->tweet_id = $tweetId;
-                    if($post["retweet_delay"] > 0){
-                        $model->scheduled_retweet_time = date("Y-m-d H:i:s", strtotime("+" . $post["retweet_delay"] . "minute"));
-                    }else{
-                        $model->scheduled_retweet_time = date("Y-m-d H:i:s");
-                    }
-                    if($post["retweet_duration"] > 0){
-                        $model->scheduled_cancel_retweet_time = date("Y-m-d H:i:s", strtotime("+" . $post["retweet_duration"] . "hour"));
-                    }
-                    $model->save();
+            $tweetIds = array($params[2]);
+        }else{
+            foreach($post["retweet_targets"] as $retweetTarget){
+                if(preg_match("/^https?:\\/\\/twitter\\.com\\/([a-zA-Z0-9_]+)\\/status\\/([0-9]+)\\/?$/", $retweetTarget, $params) > 0){
+                    $tweetIds[] = $params[2];
                 }
             }
+        }
 
-            // エラーが無かった場合、処理をコミットする。
-            Vizualizer_Database_Factory::commit($connection);
-        } catch (Exception $e) {
-            Vizualizer_Database_Factory::rollback($connection);
-            throw new Vizualizer_Exception_Database($e);
+        // リツイートのデータを登録
+        foreach($tweetIds as $tweetId){
+            $tweet = $loader->loadModel("TweetLog");
+            $tweet->findBy(array("twitter_id" => $tweetId));
+
+            // トランザクションの開始
+            $connection = Vizualizer_Database_Factory::begin("twitter");
+            try {
+                foreach($accountIds as $accountId){
+                    if($accountId != $tweet->account_id){
+                        $model = $loader->loadModel("Retweet");
+                        $model->account_id = $accountId;
+                        $model->tweet_id = $tweetId;
+                        if($post["retweet_delay"] > 0){
+                            $model->scheduled_retweet_time = date("Y-m-d H:i:s", strtotime("+" . $post["retweet_delay"] . "minute"));
+                        }else{
+                            $model->scheduled_retweet_time = date("Y-m-d H:i:s");
+                        }
+                        if($post["retweet_duration"] > 0){
+                            $model->scheduled_cancel_retweet_time = date("Y-m-d H:i:s", strtotime("+" . $post["retweet_duration"] . "hour"));
+                        }
+                        $model->save();
+                    }
+                }
+
+                // エラーが無かった場合、処理をコミットする。
+                Vizualizer_Database_Factory::commit($connection);
+            } catch (Exception $e) {
+                Vizualizer_Database_Factory::rollback($connection);
+                throw new Vizualizer_Exception_Database($e);
+            }
         }
     }
 }
