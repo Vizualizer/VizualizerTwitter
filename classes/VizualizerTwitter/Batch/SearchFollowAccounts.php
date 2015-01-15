fe2c563ad52906a020d7ad6057809a0e91c854ca
Naohisa Minagawa <info@clay-system.jp>
2015-01-15 16:13:17 +0900

    #2015011501 フォロー対象のURLにtrimを追加

--- classes/VizualizerTwitter/Batch/SearchFollowAccounts.php
+++ classes/VizualizerTwitter/Batch/SearchFollowAccounts.php
@@ -165,10 +165,10 @@ class VizualizerTwitter_Batch_SearchFollowAccounts extends Vizualizer_Plugin_Bat
 
             if(!empty($setting->follow_account)){
                 // フォローアカウントがURL形式だった場合にスクリーン名を取得
-                if (preg_match("/^https?:\\/\\/twitter.com\\/([^\\/]+)\\/?/", $setting->follow_account, $terms) > 0) {
+                if (preg_match("/^https?:\\/\\/twitter.com\\/([^\\/]+)\\/?/", trim($setting->follow_account), $terms) > 0) {
                     $screen_name = $terms[1];
                 }else{
-                    $screen_name = $setting->follow_account;
+                    $screen_name = trim($setting->follow_account);
                 }
 
                 // フォロー対象の検索処理は当日のターゲット追加数が一日のフォロー数上限の2倍以下の未満の場合のみ
