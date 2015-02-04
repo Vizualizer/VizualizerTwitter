9805067ff3dae75e46049e77b9ac8f234f0be46e
Naohisa Minagawa <info@clay-system.jp>
2015-02-04 13:59:56 +0900

    #1.9.2 Tweetのスキップの条件が複雑なため、ツイートの処理の結果を考慮するように修正

--- classes/VizualizerTwitter/Batch/FakeUser.php
+++ classes/VizualizerTwitter/Batch/FakeUser.php
@@ -118,10 +118,7 @@ class VizualizerTwitter_Batch_FakeUser extends Vizualizer_Plugin_Batch
 
             // アカウントがツイートを行う必要があるか調べる。
             $accountStatus = $account->status();
-            if ($accountStatus->tweet_status > 0 && strtotime($accountStatus->next_tweet_time) < time()) {
-                // ツイートを実行
-                $this->tweet($account);
-            } else {
+            if (time() < strtotime($accountStatus->next_tweet_time) || !$this->tweet($account)) {
                 $loader = new Vizualizer_Plugin("Twitter");
                 $model = $loader->loadModel("Retweet");
                 $retweets = $model->findAllBy(array("account_id" => $account->account_id, "le:scheduled_retweet_time" => Vizualizer::now()->date("Y-m-d H:i:s"), "retweet_time" => "0000-00-00 00:00:00"), "scheduled_retweet_time", false);
@@ -243,18 +240,24 @@ class VizualizerTwitter_Batch_FakeUser extends Vizualizer_Plugin_Batch
             if($tweetSetting->daytime_start < $tweetSetting->daytime_end && (Vizualizer::now()->date("H") < $tweetSetting->daytime_start || Vizualizer::now()->date("H") >= $tweetSetting->daytime_end)){
                 // START < ENDの場合は、その間に含まれる場合のみツイートする。
                 Vizualizer_Logger::writeInfo($account->screen_name . " : Skip tweet for daytime.");
-                return;
+                return false;
             }elseif($tweetSetting->daytime_end < $tweetSetting->daytime_start && (Vizualizer::now()->date("H") >= $tweetSetting->daytime_end && Vizualizer::now()->date("H") < $tweetSetting->daytime_start)){
                 // END < STARTの場合は、その間に含まれる場合のみツイートしない。
                 Vizualizer_Logger::writeInfo($account->screen_name . " : Skip tweet for daytime.");
-                return;
+                return false;
             }
         }
 
         // アカウントのステータスが有効のアカウントのみを対象とする。
         if ($status->tweet_status != "1" && $status->original_status != "1" && $status->advertise_status != "1" && $status->rakuten_status != "1") {
             Vizualizer_Logger::writeInfo($account->screen_name . " : Account BOT is not active.");
-            return;
+            return false;
+        }
+
+        $application = $account->application();
+        if ($application->suspended != 0) {
+            Vizualizer_Logger::writeInfo($account->screen_name . " : Application was suspended.");
+            return false;
         }
 
         $today = Vizualizer::now()->date("Y-m-d");
@@ -281,139 +284,140 @@ class VizualizerTwitter_Batch_FakeUser extends Vizualizer_Plugin_Batch
         $tweetLog->account_id = $account->account_id;
         $tweetLog->tweet_time = Vizualizer::now()->date("Y-m-d H:i:s");
 
-        $application = $account->application();
         $tweetAdvertises = $account->tweetAdvertises();
-        if ($application->suspended == 0) {
-            if ($tweetSetting->advertise_interval <= $count && $tweetAdvertises->count() > 0) {
-                $advertise = $tweetAdvertises->current()->findByPrefer();
-                if ($advertise->advertise_id > 0) {
-                    Vizualizer_Logger::writeInfo($account->screen_name . " : use advertise because " . $tweetSetting->advertise_interval . " < " . $count);
-                    // 広告を取得し記事を作成
-                    $tweetLog->tweet_id = 0;
-                    $tweetLog->tweet_type = 2;
-                    $tweetLog->tweet_text = $advertise->advertise_text;
-                    if (!empty($advertise->fixed_advertise_url)) {
-                        $tweetLog->tweet_text .= " " . $advertise->fixed_advertise_url;
-                    }
-                    Vizualizer_Logger::writeInfo($account->screen_name . " : prepare to Tweet advertise text.");
+        if ($tweetSetting->advertise_interval <= $count && $tweetAdvertises->count() > 0) {
+            $advertise = $tweetAdvertises->current()->findByPrefer();
+            if ($advertise->advertise_id > 0) {
+                Vizualizer_Logger::writeInfo($account->screen_name . " : use advertise because " . $tweetSetting->advertise_interval . " < " . $count);
+                // 広告を取得し記事を作成
+                $tweetLog->tweet_id = 0;
+                $tweetLog->tweet_type = 2;
+                $tweetLog->tweet_text = $advertise->advertise_text;
+                if (!empty($advertise->fixed_advertise_url)) {
+                    $tweetLog->tweet_text .= " " . $advertise->fixed_advertise_url;
                 }
-            } else {
-                // ツイートを取得し、記事を作成
-                $tweets = $account->tweets();
-                if ($tweets->count() > 0) {
-                    $tweet = $tweets->current()->findByPrefer();
-                    if ($tweet->tweet_id > 0) {
-                        $tweetLog->tweet_id = $tweet->tweet_id;
-                        $tweetLog->tweet_type = 1;
-                        $tweetLog->tweet_text = $tweet->tweet_text;
-                        $tweetLog->media_url = $tweet->media_url;
-                        $tweetLog->media_filename = $tweet->media_filename;
-                        Vizualizer_Logger::writeInfo($account->screen_name . " : prepare to Tweet normal text.");
-                    }
-                }
+                Vizualizer_Logger::writeInfo($account->screen_name . " : prepare to Tweet advertise text.");
             }
-
-            try {
-                if (!empty($tweetLog->tweet_text)) {
-                    if (!empty($tweetLog->media_filename)) {
-                        $params = array("status" => str_replace(" " . $tweetLog->media_url, "", $tweetLog->tweet_text));
-                        $params["media[]"] = VIZUALIZER_SITE_ROOT.Vizualizer_Configure::get("twitter_image_savepath")."/".$tweetLog->media_filename;
-                        $result = $account->getTwitter()->statuses_updateWithMedia($params);
-                    } else {
-                        $result = $account->getTwitter()->statuses_update(array("status" => $tweetLog->tweet_text));
-                    }
-                    if (isset($result->errors)) {
-                        if ($result->errors[0]->code == "261") {
-                            // アプリ自体が凍結中の場合は
-                            $connection = Vizualizer_Database_Factory::begin("twitter");
-                            try {
-                                $application = $account->application();
-                                $application->suspended = "1";
-                                $application->save();
-                                Vizualizer_Logger::writeInfo("Tweet is blocked by not writable from " . $this->user_id . " in " . $account->screen_name);
-                                Vizualizer_Database_Factory::commit($connection);
-                            } catch (Exception $e) {
-                                Vizualizer_Database_Factory::rollback($connection);
-                                throw new Vizualizer_Exception_Database($e);
-                            }
-                        } elseif ($result->errors[0]->code == "32") {
-                            // アプリ自体が凍結中の場合は
-                            $connection = Vizualizer_Database_Factory::begin("twitter");
-                            try {
-                                $application = $account->application();
-                                $application->suspended = "2";
-                                $application->save();
-                                Vizualizer_Logger::writeInfo("Tweet is blocked by frozen from " . $this->user_id . " in " . $account->screen_name);
-                                Vizualizer_Database_Factory::commit($connection);
-                            } catch (Exception $e) {
-                                Vizualizer_Database_Factory::rollback($connection);
-                                throw new Vizualizer_Exception_Database($e);
-                            }
-                        } elseif ($result->errors[0]->code == "64") {
-                            // アカウントのステータスを凍結中に変更
-                            $status->updateStatus(VizualizerTwitter_Model_AccountStatus::ACCOUNT_SUSPENDED);
-                        }else{
-                            // アカウント凍結中を解除
-                            $status->updateStatus(VizualizerTwitter_Model_AccountStatus::ACCOUNT_OK);
-                        }
-                        Vizualizer_Logger::writeError("Failed to Tweet on " . $this->user_id . " in " . $account->screen_name . " by " . print_r($result->errors, true));
-                    } elseif (!empty($result->id)) {
-                        $connection = Vizualizer_Database_Factory::begin("twitter");
-                        Vizualizer_Logger::writeInfo($account->screen_name . " : Post tweet(" . $result->id . ") : " . $tweetLog->tweet_text);
-                        $tweetLog->twitter_id = $result->id;
-                        $tweetLog->tweet_text = $result->text;
-                        if(property_exists($result->entities, "media") && is_array($result->entities->media) && count($result->entities->media) > 0){
-                            $media = $result->entities->media[0];
-                            $tweetLog->media_url = $media->url;
-                            $tweetLog->media_link = $media->media_url;
-                        }else{
-                            $tweetLog->media_url = "";
-                            $tweetLog->media_link = "";
-                        }
-                        $tweetLog->save();
-
-                        if($status->retweet_status == "1" && $tweetSetting->retweet_group_id > 0){
-                            // 強化アカウントで、対象グループが設定されている場合は、リツイートの登録も併せて行う。
-                            $group = $loader->loadModel("AccountGroup");
-                            $groups = $group->findAllBy(array("group_id" => $tweetSetting->retweet_group_id));
-                            foreach($groups as $group){
-                                if($account->account_id != $group->account_id){
-                                    $model = $loader->loadModel("Retweet");
-                                    $model->account_id = $group->account_id;
-                                    $model->tweet_id = $result->id;
-                                    if($tweetSetting->retweet_delay > 0){
-                                        $model->scheduled_retweet_time = Vizualizer::now()->strToTime("+" . $tweetSetting->retweet_delay . "minute")->date("Y-m-d H:i:s");
-                                    }else{
-                                        $model->scheduled_retweet_time = Vizualizer::now()->date("Y-m-d H:i:s");
-                                    }
-                                    if($tweetSetting->retweet_duration > 0){
-                                        $model->scheduled_cancel_retweet_time = Vizualizer::now()->strToTime("+" . $tweetSetting->retweet_duration . "hour")->date("Y-m-d H:i:s");
-                                    }
-                                    $model->save();
-                                }
-                            }
-                        }
-
-                        $interval = $tweetSetting->tweet_interval;
-                        if ($tweetSetting->wavy_flg == "1") {
-                            $interval = mt_rand(0, 20) + $interval - 10;
-                        }
-
-                        Vizualizer_Logger::writeInfo($account->screen_name . " : Use interval : " . $interval);
-                        $status->next_tweet_time = Vizualizer::now()->strToTime("+" . $interval . " minute")->date("Y-m-d H:i:s");
-                        Vizualizer_Logger::writeInfo($account->screen_name . " : Next tweet at : " . $status->next_tweet_time);
-                        $status->save();
-
-                        Vizualizer_Database_Factory::commit($connection);
-                    } else {
-                        Vizualizer_Logger::writeInfo($account->screen_name . " : error in Post tweet : " . $tweetLog->tweet_text);
-                    }
+        } else {
+            // ツイートを取得し、記事を作成
+            $tweets = $account->tweets();
+            if ($tweets->count() > 0) {
+                $tweet = $tweets->current()->findByPrefer();
+                if ($tweet->tweet_id > 0) {
+                    $tweetLog->tweet_id = $tweet->tweet_id;
+                    $tweetLog->tweet_type = 1;
+                    $tweetLog->tweet_text = $tweet->tweet_text;
+                    $tweetLog->media_url = $tweet->media_url;
+                    $tweetLog->media_filename = $tweet->media_filename;
+                    Vizualizer_Logger::writeInfo($account->screen_name . " : prepare to Tweet normal text.");
                 }
-            } catch (Exception $e) {
-                Vizualizer_Database_Factory::rollback($connection);
-                throw new Vizualizer_Exception_Database($e);
             }
         }
+
+        if (empty($tweetLog->tweet_text)) {
+            Vizualizer_Logger::writeInfo($account->screen_name . " : Tweet is empty.");
+            return false;
+        }
+
+        try {
+            if (!empty($tweetLog->media_filename)) {
+                $params = array("status" => str_replace(" " . $tweetLog->media_url, "", $tweetLog->tweet_text));
+                $params["media[]"] = VIZUALIZER_SITE_ROOT.Vizualizer_Configure::get("twitter_image_savepath")."/".$tweetLog->media_filename;
+                $result = $account->getTwitter()->statuses_updateWithMedia($params);
+            } else {
+                $result = $account->getTwitter()->statuses_update(array("status" => $tweetLog->tweet_text));
+            }
+            if (isset($result->errors)) {
+                if ($result->errors[0]->code == "261") {
+                    // アプリ自体が凍結中の場合は
+                    $connection = Vizualizer_Database_Factory::begin("twitter");
+                    try {
+                        $application = $account->application();
+                        $application->suspended = "1";
+                        $application->save();
+                        Vizualizer_Logger::writeInfo("Tweet is blocked by not writable from " . $this->user_id . " in " . $account->screen_name);
+                        Vizualizer_Database_Factory::commit($connection);
+                    } catch (Exception $e) {
+                        Vizualizer_Database_Factory::rollback($connection);
+                        throw new Vizualizer_Exception_Database($e);
+                    }
+                } elseif ($result->errors[0]->code == "32") {
+                    // アプリ自体が凍結中の場合は
+                    $connection = Vizualizer_Database_Factory::begin("twitter");
+                    try {
+                        $application = $account->application();
+                        $application->suspended = "2";
+                        $application->save();
+                        Vizualizer_Logger::writeInfo("Tweet is blocked by frozen from " . $this->user_id . " in " . $account->screen_name);
+                        Vizualizer_Database_Factory::commit($connection);
+                    } catch (Exception $e) {
+                        Vizualizer_Database_Factory::rollback($connection);
+                        throw new Vizualizer_Exception_Database($e);
+                    }
+                } elseif ($result->errors[0]->code == "64") {
+                    // アカウントのステータスを凍結中に変更
+                    $status->updateStatus(VizualizerTwitter_Model_AccountStatus::ACCOUNT_SUSPENDED);
+                }else{
+                    // アカウント凍結中を解除
+                    $status->updateStatus(VizualizerTwitter_Model_AccountStatus::ACCOUNT_OK);
+                }
+                Vizualizer_Logger::writeError("Failed to Tweet on " . $this->user_id . " in " . $account->screen_name . " by " . print_r($result->errors, true));
+            } elseif (!empty($result->id)) {
+                $connection = Vizualizer_Database_Factory::begin("twitter");
+                Vizualizer_Logger::writeInfo($account->screen_name . " : Post tweet(" . $result->id . ") : " . $tweetLog->tweet_text);
+                $tweetLog->twitter_id = $result->id;
+                $tweetLog->tweet_text = $result->text;
+                if(property_exists($result->entities, "media") && is_array($result->entities->media) && count($result->entities->media) > 0){
+                    $media = $result->entities->media[0];
+                    $tweetLog->media_url = $media->url;
+                    $tweetLog->media_link = $media->media_url;
+                }else{
+                    $tweetLog->media_url = "";
+                    $tweetLog->media_link = "";
+                }
+                $tweetLog->save();
+
+                if($status->retweet_status == "1" && $tweetSetting->retweet_group_id > 0){
+                    // 強化アカウントで、対象グループが設定されている場合は、リツイートの登録も併せて行う。
+                    $group = $loader->loadModel("AccountGroup");
+                    $groups = $group->findAllBy(array("group_id" => $tweetSetting->retweet_group_id));
+                    foreach($groups as $group){
+                        if($account->account_id != $group->account_id){
+                            $model = $loader->loadModel("Retweet");
+                            $model->account_id = $group->account_id;
+                            $model->tweet_id = $result->id;
+                            if($tweetSetting->retweet_delay > 0){
+                                $model->scheduled_retweet_time = Vizualizer::now()->strToTime("+" . $tweetSetting->retweet_delay . "minute")->date("Y-m-d H:i:s");
+                            }else{
+                                $model->scheduled_retweet_time = Vizualizer::now()->date("Y-m-d H:i:s");
+                            }
+                            if($tweetSetting->retweet_duration > 0){
+                                $model->scheduled_cancel_retweet_time = Vizualizer::now()->strToTime("+" . $tweetSetting->retweet_duration . "hour")->date("Y-m-d H:i:s");
+                            }
+                            $model->save();
+                        }
+                    }
+                }
+
+                $interval = $tweetSetting->tweet_interval;
+                if ($tweetSetting->wavy_flg == "1") {
+                    $interval = mt_rand(0, 20) + $interval - 10;
+                }
+
+                Vizualizer_Logger::writeInfo($account->screen_name . " : Use interval : " . $interval);
+                $status->next_tweet_time = Vizualizer::now()->strToTime("+" . $interval . " minute")->date("Y-m-d H:i:s");
+                Vizualizer_Logger::writeInfo($account->screen_name . " : Next tweet at : " . $status->next_tweet_time);
+                $status->save();
+
+                Vizualizer_Database_Factory::commit($connection);
+            } else {
+                Vizualizer_Logger::writeInfo($account->screen_name . " : error in Post tweet : " . $tweetLog->tweet_text);
+            }
+        } catch (Exception $e) {
+            Vizualizer_Database_Factory::rollback($connection);
+            throw new Vizualizer_Exception_Database($e);
+        }
+        return true;
     }
 
     /**
